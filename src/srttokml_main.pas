e4d9d01b051cc8fbc2975d7f27bdfe0968e86776
H. Elsner <48010199+h-elsner@users.noreply.github.com>
2023-08-27 21:46:29 +0200

    Add: GPX and Drag&Drop

--- src/srttokml_main.pas
+++ src/srttokml_main.pas
@@ -40,6 +40,7 @@ History
 2023-08-19 Idea and plan, GUI
 2023-08-20 V0.1 First version
 2023-08-20 V0.2 Extrude selectable
+2023-08-27 V0.3 GPX added, Drag&Drop
 *)
 
 unit SRTtoKML_main;
@@ -76,21 +77,28 @@ type
     Memo2: TMemo;
     OpenDialog1: TOpenDialog;
     panTop: TPanel;
+    rgMode: TRadioGroup;
     SaveDialog1: TSaveDialog;
     Splitter1: TSplitter;
     XMLPropStorage1: TXMLPropStorage;
     procedure btnCloseClick(Sender: TObject);
     procedure btnConvertClick(Sender: TObject);
     procedure btnSaveClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDropFiles(Sender: TObject; const FileNames: array of string);
+    procedure rgModeClick(Sender: TObject);
   private
     procedure KMLheader(f, dt: string; klist: TStringList);
+    procedure DoWithFile(const fn: string);
   public
-
+    procedure MakeKML(fnk: string);
+    procedure MakeGPX(fnk: string);
   end;
 
 const
-  vers='0.2 (08/2023)';
+  AppShort='SRTtoKML';
+  vers='0.3 (08/2023)';
   latID='latitude: ';
   lonID='longitude: ';
   altID='abs_alt: ';
@@ -104,8 +112,9 @@ const
   kmlvers='<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">';
   pmtag='Placemark>';
   doctag='Document>';
+  gpxvers='<gpx version="1.0">';
 
-  {How flight trck looks like}
+  {How flight tarck looks like}
   aircrafticon='https://earth.google.com/images/kml-icons/track-directional/track-0.png';
   ColorTrack=clYellow;
   ColorWaypoints=clRed;
@@ -119,8 +128,8 @@ implementation
 
 {$R *.lfm}
 
-{$I SRTtoKML_de.inc}                                     {German GUI}
-{ $I SRTtoKML_en.inc}                                    {English GUI}
+{ $I SRTtoKML_de.inc}                                     {German GUI}
+{$I SRTtoKML_en.inc}                                    {English GUI}
 
 { TForm1 }
 
@@ -134,7 +143,7 @@ begin
   result:=tab2+'<name>'+nm+'</name>';
 end;
 
-function write_des(des: string): string;
+function write_des(des: string): string; inline;
 begin
   result:=tab2+'<description>'+des+'</description>';
 end;
@@ -144,8 +153,8 @@ begin
   klist.Add(xmlvers);
   klist.Add(kmlvers);
   klist.Add('<'+doctag);
-  klist.Add(write_nme('SRTtoKML'));
-  klist.Add(write_des(dt+' - '+ExtractFileName(f)));
+  klist.Add(write_nme(appShort));
+  klist.Add(write_des(dt+' - '+f));
   klist.Add('<Style id="Flightpath">');
   klist.Add(tab2+'<LineStyle>');
   klist.Add(tab4+'<color>'+ColorToKMLColor(ColorTrack)+'</color>');  {Color for track}
@@ -192,17 +201,17 @@ begin
       p.time:=s.Split([tab1])[1];
       result:=true;
     end else begin
-      sellist.DelimitedText:=s;
       if pos(selpar, s)>0 then begin                     {Get lat, lon, alt}
+        sellist.DelimitedText:=s;
         for i:=0 to sellist.Count-1 do begin
           if pos(latID, sellist[i])>0 then begin
-            p.lat:=trim(sellist[i].Split([selval])[1]);
+            p.lat:=sellist[i].Split([selval])[1];
           end;
           if pos(lonID, sellist[i])>0 then begin
-            p.lon:=trim(sellist[i].Split([selval])[1]);
+            p.lon:=sellist[i].Split([selval])[1];
           end;
           if pos(altID, sellist[i])>0 then begin
-            p.alt:=trim(sellist[i].Split([selval])[1]);  {abs_alt on second position}
+            p.alt:=sellist[i].Split([selval])[1];        {abs_alt on second position}
             result:=true;
           end;
         end;
@@ -215,6 +224,31 @@ begin
 end;
 
 procedure TForm1.btnConvertClick(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+    DoWithFile(OpenDialog1.FileName);
+end;
+
+procedure TForm1.FormDropFiles(Sender: TObject; const FileNames: array of string);
+begin
+  if FileExists(FileNames[0]) then
+    DoWithFile(FileNames[0]);
+end;
+
+procedure TForm1.DoWithFile(const fn: string);
+begin
+  Memo1.Lines.Clear;
+  Memo2.Lines.Clear;
+  Memo1.Lines.LoadFromFile(fn);                          {Load SRT file}
+  if Memo1.Lines.Count>100 then begin
+    case rgMode.Itemindex of
+      0: MakeKML(ChangeFileExt(ExtractFileName(fn), ''));
+      1: MakeGPX(ChangeFileExt(ExtractFileName(fn), ''));
+    end;
+  end;
+end;
+
+procedure TForm1.MakeKML(fnk: string);
 var
   kmllist, coolist: TStringList;
   i: integer;
@@ -222,77 +256,121 @@ var
   ex: char;
 
 begin
-  if OpenDialog1.Execute then begin
-    if cbExtrude.Checked then
-      ex:='1'
-    else
-      ex:='0';
-    kmllist:=TStringList.Create;
-    coolist:=TStringList.Create;
-    Screen.Cursor:=crHourGlass;
-    ClearFPoint(dp);
-    try
-      Memo1.Lines.Clear;
-      Memo2.Lines.Clear;
-      Memo1.Lines.LoadFromFile(OpenDialog1.FileName);    {Load SRT file}
-      if Memo1.Lines.Count>100 then begin
-        for i:=1 to 10 do begin                          {Get start point and first data set}
-          if not GetFPoint(Memo1.Lines[i], dp) then
-            continue;
-          ValidateFPoint(dp);
-          if dp.v then
-            break;
-        end;
+  if cbExtrude.Checked then
+    ex:='1'
+  else
+    ex:='0';
+  kmllist:=TStringList.Create;
+  coolist:=TStringList.Create;
+  Screen.Cursor:=crHourGlass;
+  ClearFPoint(dp);
+  try
+    for i:=1 to 10 do begin                              {Get start point and first data set}
+      if not GetFPoint(Memo1.Lines[i], dp) then
+        continue;
+      ValidateFPoint(dp);
+      if dp.v then
+        break;
+    end;
+    if dp.v then begin
+      KMLheader(fnk, dp.date, kmllist);
+      kmllist.Add('<'+pmtag);
+      kmllist.Add(write_nme(FlightPathName));
+      kmllist.Add(write_des(fnk));
+      kmllist.Add(tab2+'<styleUrl>#Flightpath</styleUrl>');
+      kmllist.Add(tab2+'<gx:Track>');
+      kmllist.Add(tab4+'<altitudeMode>absolute</altitudeMode>');  {This is fix, no choice}
+      kmllist.Add(tab4+'<extrude>'+ex+'</extrude>');
+
+      ClearFPoint(dp);
+      for i:=0 to Memo1.Lines.Count-1 do begin
+        GetFPoint(Memo1.Lines[i], dp);
+        ValidateFPoint(dp);
         if dp.v then begin
-          KMLheader(ChangeFileExt(OpenDialog1.FileName, ''), dp.date, kmllist);
-          kmllist.Add('<'+pmtag);
-          kmllist.Add(write_nme(FlightPathName));
-          kmllist.Add(write_des(ExtractFileName(OpenDialog1.FileName)));
-          kmllist.Add(tab2+'<styleUrl>#Flightpath</styleUrl>');
-          kmllist.Add(tab2+'<gx:Track>');
-          kmllist.Add(tab4+'<altitudeMode>absolute</altitudeMode>');  {This is fix, no choice}
-          kmllist.Add(tab4+'<extrude>'+ex+'</extrude>');
-
-          ClearFPoint(dp);
-          for i:=0 to Memo1.Lines.Count-1 do begin
-            GetFPoint(Memo1.Lines[i], dp);
-            ValidateFPoint(dp);
-            if dp.v then begin
-              coolist.Add(tab4+'<gx:coord>'+dp.lon+tab1+dp.lat+tab1+dp.alt+'</gx:coord>');
-              kmllist.Add(tab4+'<when>'+dp.date+'T'+dp.time+'Z</when>');
-              dp.v:=false;
-            end;
-          end;
-
-          if coolist.Count>10 then begin                 {The last point is valid and landing placemark}
-            for i:=0 to coolist.Count-1 do
-              kmllist.Add(coolist[i]);
-            kmllist.Add('</gx:Track>');
-            kmllist.Add('</'+pmtag);
-            kmllist.Add('</'+doctag);
-            kmllist.Add('</kml>');
-            Memo2.Lines.Assign(kmllist);
-          end;
+          coolist.Add(tab4+'<gx:coord>'+dp.lon+tab1+dp.lat+tab1+dp.alt+'</gx:coord>');
+          kmllist.Add(tab4+'<when>'+dp.date+'T'+dp.time+'Z</when>');
+          dp.v:=false;
         end;
       end;
-    finally
-      if Memo2.Lines.Count>1 then
-        btnSave.Enabled:=true;                           {Save needed}
-      Screen.Cursor:=crDefault;
-      kmllist.Free;
-      coolist.Free;
-    end;
 
+      if coolist.Count>10 then begin                     {The last point is valid and landing placemark}
+        for i:=0 to coolist.Count-1 do
+          kmllist.Add(coolist[i]);
+        kmllist.Add('</gx:Track>');
+        kmllist.Add('</'+pmtag);
+        kmllist.Add('</'+doctag);
+        kmllist.Add('</kml>');
+        Memo2.Lines.Assign(kmllist);
+      end;
+    end;
+  finally
+    if Memo2.Lines.Count>1 then
+      btnSave.Enabled:=true;                             {Save needed}
+    Screen.Cursor:=crDefault;
+    kmllist.Free;
+    coolist.Free;
+  end;
+end;
+
+procedure TForm1.MakeGPX(fnk: string);
+var
+  gpxlist: TStringList;
+  i: integer;
+  dp: fpoint;
+
+begin
+  gpxlist:=TStringList.Create;
+  Screen.Cursor:=crHourGlass;
+  ClearFPoint(dp);
+  try
+    for i:=1 to 10 do begin                              {Get start point and first data set}
+      if not GetFPoint(Memo1.Lines[i], dp) then          {Validate and get begin of recording}
+        continue;
+      ValidateFPoint(dp);
+      if dp.v then
+        break;
+    end;
+    if dp.v then begin
+      ClearFPoint(dp);
+      gpxlist.Add(xmlvers+gpxvers);                      {Header of GPX file}
+      gpxlist.Add(write_nme(FlightPathName));
+      gpxlist.Add('<trk>'+write_nme(fnk)+'<trkseg>');
+      for i:=0 to Memo1.Lines.Count-1 do begin
+        GetFPoint(Memo1.Lines[i], dp);
+        ValidateFPoint(dp);
+        if dp.v then begin
+          gpxlist.Add(tab2+'<trkpt lat="'+dp.lat+'" lon="'+dp.lon+
+                      '"><ele>'+dp.alt+'</ele>');
+          gpxlist.Add(tab4+'<Time>'+dp.date+'T'+dp.time+'Z</time></trkpt>');
+          dp.v:=false;
+        end;
+      end;
+      gpxlist.Add('</trkseg></trk></gpx>');              {End tack and close GPX file}
+      Memo2.Lines.Assign(gpxlist);
+    end;
+  finally
+    if Memo2.Lines.Count>1 then
+      btnSave.Enabled:=true;                             {Save needed}
+    Screen.Cursor:=crDefault;
+    gpxlist.Free;
   end;
 end;
 
 procedure TForm1.btnSaveClick(Sender: TObject);
 begin
-  SaveDialog1.Filename:=ChangeFileExt(OpenDialog1.FileName, '.kml');
+  case rgMode.ItemIndex of
+    0: SaveDialog1.Filename:=ChangeFileExt(OpenDialog1.FileName, '.kml');
+    1: SaveDialog1.Filename:=ChangeFileExt(OpenDialog1.FileName, '.gpx');
+  end;
   if SaveDialog1.Execute then begin
     Memo2.Lines.SaveToFile(SaveDialog1.FileName);
     btnSave.Enabled:=false;                              {ID already saved}
   end;
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  cbExtrude.Enabled:=(rgMode.ItemIndex=0);
 end;
 
 procedure TForm1.btnCloseClick(Sender: TObject);
@@ -313,12 +391,19 @@ begin
   btnClose.Hint:=hntClose;
   btnSave.Caption:=capSave;
   btnSave.Hint:=hntSave;
+  rgMode.Caption:=capMode;
+  rgMode.Hint:=hntMode;
   cbExtrude.Hint:=hntExtrude;
   OpenDialog1.Title:=titOpen;
   SaveDialog1.Title:=titSave;
   Memo1.Text:=hntConvert;
   Memo2.Text:=hntSave;
   btnSave.Enabled:=false;                                 {ID save not needed}
+end;
+
+procedure TForm1.rgModeClick(Sender: TObject);
+begin
+  cbExtrude.Enabled:=(rgMode.ItemIndex=0);
 end;
 
 end.
